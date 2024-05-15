e588b3a12228398bff812ffe67ccbafa5f9e8f36
chaeyoon <chaeyoon@linecorp.com>
2024-05-15 12:01:39 +0900

    Apply navigation

--- app/src/main/java/com/chaeyoon/haebit/MainActivity.kt
+++ app/src/main/java/com/chaeyoon/haebit/MainActivity.kt
@@ -5,44 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.commit
 import com.chaeyoon.haebit.databinding.ActivityMainBinding
-import com.chaeyoon.haebit.obscura.CameraFragment
 
 class MainActivity : AppCompatActivity() {
-    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        initFragmentResultListener()
-        openCameraFragment()
-    }
-
-    private fun initFragmentResultListener() {
-        supportFragmentManager.setFragmentResultListener(
-            REQUEST_KEY_FINISH,
-            this
-        ) { key, _ ->
-            if (key != REQUEST_KEY_FINISH) return@setFragmentResultListener
-            finish()
-        }
-    }
-
-    private fun openCameraFragment() {
-        supportFragmentManager.commit {
-            val cameraFragment = CameraFragment.newInstance(REQUEST_KEY_FINISH)
-            replace(R.id.fragment_container, cameraFragment)
-        }
-    }
-
-    companion object {
-        private const val REQUEST_KEY_FINISH = "REQUEST_KEY_FINISH"
     }
 }
--- app/src/main/java/com/chaeyoon/haebit/obscura/CameraFragment.kt
+++ app/src/main/java/com/chaeyoon/haebit/obscura/CameraFragment.kt
@@ -3,8 +3,6 @@ package com.chaeyoon.haebit.obscura
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
-import androidx.core.os.bundleOf
-import androidx.fragment.app.setFragmentResult
 import com.chaeyoon.haebit.R
 import com.chaeyoon.haebit.databinding.FragmentCameraBinding
 import com.chaeyoon.haebit.permission.PermissionChecker
@@ -18,12 +16,6 @@ class CameraFragment : Fragment(R.layout.fragment_camera) {
 
     private lateinit var permissionChecker: PermissionChecker
 
-    private val finishRequestKey by lazy {
-        requireNotNull(
-            arguments?.getString(ARG_FINISH_REQUEST_KEY)
-        )
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         permissionChecker = PermissionChecker(this)
@@ -32,19 +24,5 @@ class CameraFragment : Fragment(R.layout.fragment_camera) {
     override fun onStart() {
         super.onStart()
         permissionChecker.checkCameraPermissions()
-    }
-
-    private fun finishCamera() {
-        setFragmentResult(finishRequestKey, bundleOf())
-    }
-
-    companion object {
-        fun newInstance(
-            finishResultKey: String
-        ) = CameraFragment().apply {
-            arguments = bundleOf(ARG_FINISH_REQUEST_KEY to finishResultKey)
-        }
-
-        private const val ARG_FINISH_REQUEST_KEY = "ARG_FINISH_REQUEST_KEY"
     }
 }
