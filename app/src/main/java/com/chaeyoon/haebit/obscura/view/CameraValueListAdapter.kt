da414090008babd26615aa40b7639c1c0f331c23
yoonchae125 <yoonchae125@naver.com>
2024-06-07 20:07:02 +0900

    Apply font

--- app/src/main/java/com/chaeyoon/haebit/obscura/view/CameraValueListAdapter.kt
+++ app/src/main/java/com/chaeyoon/haebit/obscura/view/CameraValueListAdapter.kt
@@ -1,12 +1,18 @@
 package com.chaeyoon.haebit.obscura.view
 
+import android.content.Context
+import android.graphics.Typeface
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.FontRes
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.chaeyoon.haebit.R
 import com.chaeyoon.haebit.databinding.ViewCameraValueItemBinding
+import com.chaeyoon.haebit.databinding.ViewShutterSpeedItemBinding
 import com.chaeyoon.haebit.obscura.utils.constants.shutterSpeedStringValues
 import com.chaeyoon.haebit.obscura.utils.extensions.toOneDecimalPlaces
 
@@ -20,18 +26,34 @@ class CameraValueListAdapter(private val type: Type) :
         parent: ViewGroup,
         viewType: Int
     ): CameraValueViewHolder {
-        val binding = ViewCameraValueItemBinding.inflate(
-            LayoutInflater.from(parent.context),
-            parent,
-            false
-        )
 
         return when (type) {
-            Type.APERTURE -> ApertureViewHolder(binding)
+            Type.APERTURE -> {
+                val binding = ViewCameraValueItemBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                ApertureViewHolder(binding)
+            }
 
-            Type.SHUTTER_SPEED -> ShutterSpeedViewHolder(binding)
+            Type.SHUTTER_SPEED -> {
+                val binding = ViewShutterSpeedItemBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                ShutterSpeedViewHolder(binding)
+            }
 
-            Type.ISO -> IsoViewHolder(binding)
+            Type.ISO -> {
+                val binding = ViewCameraValueItemBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                IsoViewHolder(binding)
+            }
         }
     }
 
@@ -47,26 +69,49 @@ class CameraValueListAdapter(private val type: Type) :
         private val binding: ViewCameraValueItemBinding
     ) : CameraValueViewHolder(binding.root) {
         override fun bind(value: Float) {
+            val typeface =
+                binding.root.context.getTypeface(
+                    R.font.reddit_mono_semi_bold,
+                    "reddit_mono_semi_bold.ttf"
+                )
+            binding.valueText.typeface = typeface
             binding.valueText.text = format(value)
         }
 
         private fun format(value: Float) = "ƒ${value.toOneDecimalPlaces()}"
     }
 
-    inner class ShutterSpeedViewHolder(
-        private val binding: ViewCameraValueItemBinding
-    ) : CameraValueViewHolder(binding.root) {
-        override fun bind(value: Float) {
-            binding.valueText.text = format()
+    private fun Context.getTypeface(@FontRes resId: Int, path: String) =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            resources.getFont(resId)
+        } else {
+            Typeface.createFromAsset(resources.assets, path)
         }
 
-        private fun format() = "${shutterSpeedStringValues[adapterPosition]}S"
+    inner class ShutterSpeedViewHolder(
+        private val binding: ViewShutterSpeedItemBinding
+    ) : CameraValueViewHolder(binding.root) {
+        override fun bind(value: Float) {
+            val typeface =
+                binding.root.context.getTypeface(
+                    R.font.frank_ruhl_libre_extra_bold,
+                    "frank_ruhl_libre_extra_bold.ttf"
+                )
+            binding.valueText.typeface = typeface
+            binding.valueText.text = shutterSpeedStringValues[adapterPosition]
+        }
     }
 
     inner class IsoViewHolder(
         private val binding: ViewCameraValueItemBinding
     ) : CameraValueViewHolder(binding.root) {
         override fun bind(value: Float) {
+            val typeface =
+                binding.root.context.getTypeface(
+                    R.font.frank_ruhl_libre_extra_bold,
+                    "frank_ruhl_libre_extra_bold.ttf"
+                )
+            binding.valueText.typeface = typeface
             binding.valueText.text = format(value)
         }
 
