96bcbb9649e66feaec8e9670b59e5369b4ad47bc
yoonchae125 <yoonchae125@naver.com>
2025-01-09 18:44:01 +0900

    Remove compilerOptions

--- lightmeter/build.gradle.kts
+++ lightmeter/build.gradle.kts
@@ -2,12 +2,6 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinJvm)
 }
 
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8
-    }
-}
-
 dependencies {
     testImplementation(libs.junit)
 }
