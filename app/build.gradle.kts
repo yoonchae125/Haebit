dbbfe5c779bd20efa7daaa5782b2bf9dc376c4e2
yoonchae125 <yoonchae125@naver.com>
2025-01-09 16:42:18 +0900

    Update app name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.chaeyoon.haebit"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 4
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
